f78dddd0ad17997117ec6f31d2590baa895bb1d8
steeven-louk <steevenlouk.sl@gmail.com>
2025-07-18 18:35:21 +0200

    ajout de react-toastify pour la gestion des notification

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+  import { ToastContainer } from 'react-toastify';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ToastContainer />
     </QueryClientProvider>
   </StrictMode>,
 )
--- src/services/API/taskService.ts
+++ src/services/API/taskService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import type { Task } from '../../types/taskType';
+import { toast } from 'react-toastify';
 
 const API_URL = 'http://localhost:3000/api/tasks';
 
@@ -8,12 +9,14 @@ export const fetchTasks = async () => {
         const response = await axios.get(API_URL);
         console.log("Fetched tasks 1:", response.data);
         if(response.status === 200){
+            toast.success(response.data.message);
             const {data} = response.data;
             const todos: Task[] = Array.isArray(data) ? data : [];
             return todos;
         }
 
     } catch (error) {
+        toast.error(error?.response?.data.message || 'Erreur lors de la récupération des tâches',)
         console.error(error?.response?.data.message || 'Erreur lors de la récupération des tâches');
         // throw error;
     }
@@ -47,19 +50,22 @@ export const addTask = async (task:Omit<Task,'id'|'createdAt'>) => {
         status: task.status,
     });
     if(response.status ===201){
+        toast.success(response.data.message)
         const {data} = response.data;
         return data
     }
     } catch (error) {
         console.log(error)
+        toast.error(error?.response?.data.message)
     }
 };
 
 export const deleteTask = async (id:number) => {
     try {
-       await axios.delete(`${API_URL}/${id}`);
-
+      const response = await axios.delete(`${API_URL}/${id}`);
+        toast.success(response.data.message)
     } catch (error) {
+        toast.error(error?.response?.data.message || 'Erreur lors de la suppression de la tâche');
         console.error(error?.response?.data.message || 'Erreur lors de la suppression de la task');
     }
 };
@@ -69,10 +75,12 @@ export const updateTask = async (id:number, task:Task) => {
         const response = await axios.put(`${API_URL}/${id}`, task);
         console.log("Updated task:", response);
         if(response.status === 200){
+            toast.success(response.data.message);
             const {data} = response.data;
             return data;
         }
     } catch (error) {
+        toast.error(error?.response?.data.message || 'Erreur lors de la mise à jour de la tâche');
         console.error(error?.response?.data.message || 'Erreur lors de la mise à jour de la tâche');
         throw error;
     }
@@ -81,13 +89,15 @@ export const updateTask = async (id:number, task:Task) => {
 export const updateTaskStatus = async (id:number, status:string) => {
     try {
         const response = await axios.patch(`${API_URL}/${id}/complete`, { status });
-        console.log("Updated task status:", response);
+        // console.log("Updated task status:", response);
         if(response.status === 200){
+            toast.success(response.data.message);
             const {data} = response.data;
             return data;
         }
         // return response.data;
     } catch (error) {
+        toast.error(error?.response?.data.message || 'Erreur lors de la mise à jour du statut de la tâche');
         console.error(error?.response?.data.message || 'Erreur lors de la mise à jour du statut de la tâche');
         throw error;
     }
